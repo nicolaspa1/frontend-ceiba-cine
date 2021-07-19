5c7ac7351b49798d3555c5eb4f0483746ad2a373
Nicolas Pinzon <nicolas.pinzon@ceiba.com.co>
2021-07-19 15:11:53 -0500

    Mejora del banner

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,14 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-// import Swiper from 'swiper';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  title = 'Bienvenido al cine' ;
 
   constructor() { }
 
